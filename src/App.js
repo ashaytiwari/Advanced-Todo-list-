bdb985badb952e76a17735f97d0578a6f44adb2a
ashaytiwari <ashaytiwari2001@gmail.com>
2020-09-27 04:58:28 +0530

    reafactor: code refactor in whole project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import TodoPage from './components/TodoPage';
+// import LoginPage from './components/LoginPage';
+// import TodoPage from './components/TodoPage';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Switch>
           <Route path='/' exact component={() => <LoginPage />} />
           <Route path='/todoPage' exact component={() => <TodoPage />} />
         </Switch>
-      </Router>
+      </Router> */}
     </div>
   );
 }

bbb40a14432df3f5221b9236df8f33770ceb8469
ashaytiwari <ashaytiwari2001@gmail.com>
2020-09-29 13:28:42 +0530

    feat: login page designing

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { 
     Box,
     Grid, 
-    Paper
+    Paper,
+    Typography
 } from '@material-ui/core';
 import styles from './styles'
 
@@ -14,7 +15,15 @@ const LoginPage = () => {
             <Grid container>
                 <Grid item md={1} lg={1} xl={1}></Grid>
                 <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
-                    <Paper>hello</Paper>
+                    <Paper className={classes.loginCard} elevation={10}>
+                        <Grid container>
+                            <Grid item md={7} lg={8} xl={8} className={classes.welcomeSection}>
+                                <Typography className={classes.greet}>Welcome to</Typography>
+                                <Typography className={classes.title}>DevBand</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={5} lg={4} xl={4}></Grid>
+                        </Grid>
+                    </Paper>
                 </Grid>
                 <Grid item md={1} lg={1} xl={1}></Grid>
             </Grid>
--- src/Pages/LoginPage/styles.js
+++ src/Pages/LoginPage/styles.js
@@ -1,19 +1,46 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const styles = makeStyles((theme) => ({
-    
-        loginPage: {
-            background: '#000428', 
-            background: '-webkit-linear-gradient(to top right, #000428,#004e92 )',  
-            background: 'linear-gradient(to top right, #000428,#004e92 )', 
-            height:'100%',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            padding: '1px',
-            width: '100%'
-        },
-        
+    loginPage: {
+        background: '#000428',
+        background: '-webkit-linear-gradient(to top right, #000428,#004e92 )',
+        background: 'linear-gradient(to top right, #000428,#004e92 )',
+        height: '100%',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        padding: '1px',
+        width: '100%'
+    },
+    loginCard: {
+        border: 'none',
+        borderRadius: '0',
+        padding: '0',
+        width: '100%',
+        height: '500px',
+        marginTop: '70px'
+    },
+    welcomeSection: {
+        background: '#000428',
+        background: '-webkit-linear-gradient(to top right, #000428,#004e92 )',
+        background: 'linear-gradient(to top right, #000428,#004e92 )',
+        height: '500px',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        padding: "100px"
+    },
+    greet: {
+        color: "#ffffff",
+        fontSize: '35px',
+        letterSpacing: '4px',
+    },
+    title: {
+        color: '#ffffff',
+        fontSize: '50px',
+        fontFamily: 'Ubuntu',
+        letterSpacing: '4px',
+    },
 
 }))
 
